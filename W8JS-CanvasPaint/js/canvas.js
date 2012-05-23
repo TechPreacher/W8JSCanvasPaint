ebb5aca098a180b378dca3e3a852b015bed2cab1
Sascha Corti <sascha@corti.com>
2012-05-23 10:14:48 +0200

    Initial Check-In

---
+++ W8JS-CanvasPaint/js/canvas.js
@@ -0,0 +1,137 @@
+// canvas.js is adapted from wiliammalone's excellent 
+// "Create a Drawing App with HTML5 Canvas and JavaScript" project. 
+// (http://www.williammalone.com/articles/create-html5-canvas-javascript-drawing-app/).
+
+// public members
+var curColor;
+
+var drawingApp = (function () {
+
+    "use strict";
+
+    var canvas,
+		context,
+		clickX = [],
+		clickY = [],
+        colorRed = "#ff0000",
+        colorGreen = "#00ff00",
+        colorBlue = "#0000ff",
+        clickColor = new Array(),
+        clickDrag = [],
+		paint = false;
+
+    curColor = colorRed;
+
+    // Redraws the canvas.
+    function redraw() {
+        
+        context.lineJoin = "round";
+        context.lineWidth = 5;
+
+        for (var i = 0; i < clickX.length; i++) {
+            context.beginPath();
+            if (clickDrag[i] && i) {
+                context.moveTo(clickX[i - 1], clickY[i - 1]);
+            }
+            else {
+                context.moveTo(clickX[i] - 1, clickY[i]);
+            }
+            context.lineTo(clickX[i], clickY[i]);
+            context.closePath();
+            context.strokeStyle = clickColor[i];
+            context.stroke();
+        }
+    };
+
+    // Adds a point to the drawing array.
+    // @param x
+    // @param y
+    // @param dragging
+    function addClick(x, y, dragging) {
+
+        clickX.push(x);
+        clickY.push(y);
+        clickDrag.push(dragging);
+        clickColor.push(curColor);
+    };
+
+    // Add mouse and touch event listeners to the canvas
+    function createUserEvents() {
+
+        // Add resize event handler to window.
+        window.addEventListener("resize", resized, false);
+
+        // Add mouse event listeners to canvas element
+        canvas.addEventListener("mousedown", press, false);
+        canvas.addEventListener("mousemove", drag, false);
+        canvas.addEventListener("mouseup", release);
+        canvas.addEventListener("mouseout", cancel, false);
+
+        // Add touch event listeners to canvas element
+        canvas.addEventListener("touchstart", press, false);
+        canvas.addEventListener("touchmove", drag, false);
+        canvas.addEventListener("touchend", release, false);
+        canvas.addEventListener("touchcancel", cancel, false);
+    };
+
+    // Event Handlers
+    function press(e) {
+        var mouseX = e.pageX - this.offsetLeft;
+        var mouseY = e.pageY - this.offsetTop;
+
+        paint = true;
+        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+        redraw();
+    };
+
+    function drag(e) {
+        if (paint) {
+            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+            redraw();
+        }
+    };
+
+    function release(e) {
+        paint = false;
+    };
+
+    function cancel(e) {
+        paint = false;
+    };
+
+    function resized() {
+        canvas.setAttribute('width', window.innerWidth - 20 + "px");
+        canvas.setAttribute('height', window.innerHeight - 20 + "px");
+        redraw();
+    };
+
+    function setColorRed() {
+        curColor = colorRed;
+        redraw();
+    };
+
+    function setColorGreen() {
+        curColor = colorGreen;
+        redraw();
+    };
+
+    function setColorBlue() {
+        curColor = colorBlue;
+        redraw();
+    };
+
+    // Creates a canvas element, loads images, adds events, and draws the canvas for the first time.
+    function init() {
+        canvas = document.getElementById('canvas');
+        context = canvas.getContext('2d');
+        createUserEvents();
+        resized();
+    };
+
+    return {
+        init: init,
+        setColorRed: setColorRed,
+        setColorGreen: setColorGreen,
+        setColorBlue: setColorBlue
+    };
+}());
---
+++ W8JS-CanvasPaint/js/default.js
@@ -0,0 +1,99 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    var app = WinJS.Application;
+
+    app.onactivated = function (eventObject) {
+        if (eventObject.detail.kind === Windows.ApplicationModel.Activation.ActivationKind.launch) {
+            if (eventObject.detail.previousExecutionState !== Windows.ApplicationModel.Activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize 
+                // your application here.
+            } else {
+                // TODO: This application has been reactivated from suspension. 
+                // Restore application state here.
+            }
+            WinJS.UI.processAll();
+
+            drawingApp.init();
+
+            // Import File
+            document.getElementById("importButton").addEventListener("click", function () {
+                var img = new Image();
+                img.onload = function () {
+                    setTimeout(function () {
+                        var scaleFactor = Math.max(window.innerHeight / img.naturalHeight, window.innerWidth / img.naturalWidth);
+                        document.getElementById("canvas").getContext("2d").drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight, 0, 0, img.naturalWidth * scaleFactor, img.naturalHeight * scaleFactor);
+                    }, 100);
+                };
+
+                var picker = new Windows.Storage.Pickers.FileOpenPicker();
+                picker.fileTypeFilter.replaceAll([".jpg", ".bmp", ".gif", ".png"]);
+                picker.pickSingleFileAsync().then(function (file) {
+                    var blobUrl = URL.createObjectURL(file);
+                    img.src = blobUrl;
+                });
+
+            });
+
+            // Save File
+            document.getElementById("exportButton").addEventListener("click", function () {
+                var blob = document.getElementById("canvas").msToBlob();
+                // Next line needs "Pictures Libary" access capability.
+                var picker = new Windows.Storage.Pickers.FileSavePicker();
+                picker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.picturesLibrary;
+                picker.fileTypeChoices.insert("PNG", [".png"]);
+                picker.pickSaveFileAsync().then(function (file) {
+                    file.openAsync(Windows.Storage.FileAccessMode.readWrite).then(function (stream) {
+                        var input = blob.msDetachStream();
+                        var output = stream.getOutputStreamAt(0);
+                        Windows.Storage.Streams.RandomAccessStream.copyAsync(input, output).then(function () {
+                            output.flushAsync().then(function () {
+                                input.close();
+                                output.close();
+                                Windows.UI.Popups.MessageDialog("File saved.").showAsync().then(function () {});
+                            });
+                        });
+                    });
+                });
+            });
+
+            // Share Contract
+            Windows.ApplicationModel.DataTransfer.DataTransferManager.getForCurrentView().addEventListener("datarequested", function (e) {
+                var request = e.request;
+                var blob = document.getElementById("canvas").msToBlob();
+
+                request.data.properties.title = "CanvasPaint doodle";
+                request.data.properties.description = "Share image from CanvasPaint";
+                request.data.properties.applicationName = "CanvasPaint";
+
+                var streamReference = Windows.Storage.Streams.RandomAccessStreamReference.createFromStream(blob.msDetachStream());
+                request.data.properties.thumbnail = streamReference;
+                request.data.setBitmap(streamReference);
+
+            });
+
+            // Load last color used.
+            if (Windows.Storage.ApplicationData.current.localSettings.values["latestColor"] != null) {
+                curColor = Windows.Storage.ApplicationData.current.localSettings.values["latestColor"];
+            }
+
+        }
+    };
+
+    app.oncheckpoint = function (eventObject) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the 
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // eventObject.setPromise().
+
+        // Save last color used.
+        Windows.Storage.ApplicationData.current.localSettings.values["latestColor"] = curColor;
+    };
+
+    app.start();
+
+})();
